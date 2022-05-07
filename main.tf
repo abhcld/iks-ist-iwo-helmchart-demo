7cfc7537403be1be9c17dfafb1630949674481d8
abhcld <104119614+abhcld@users.noreply.github.com>
2022-05-07 20:59:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -62,18 +62,18 @@ client_key = base64decode(yamldecode(base64decode(data.intersight_kubernetes_clu
 resource helm_release helloiksfrtfcb {
   name       = "iwocollector"
   namespace = "iwo-collector"
-  chart = "https://github.com/CiscodCloud/helm_chart/raw/main/iwok8scollector-0.6.2.tgz"
+  chart = "https://github.com/abhcld/iks-ist-iwo-helmchart-demo/raw/main/iwo-k8s-collector-v1.0.1.tgz"
   set {
     name  = "iwoServerVersion"
-    value = "8.0"
+    value = "8.5"
   }
   set {
     name  = "collectorImage.tag"
-    value = "8.0.6"
+    value = "8.5.1"
   }
   set {
     name  = "targetName"
-    value = local.clustername
+    value = "kubernetes-cluster01"
   }
 }
 
